7f00a5989892ef23bd003d53005d2b9e55a1081f
PiiKay <144244080+Pernilla72@users.noreply.github.com>
2024-10-07 10:49:42 +0200

    saving changes, building further on the assigments

--- LINQLabExercice/Program.cs
+++ LINQLabExercice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DocumentFormat.OpenXml.ExtendedProperties;
+using DocumentFormat.OpenXml.Spreadsheet;
 using System;
 using System.Diagnostics;
 
@@ -22,13 +23,18 @@ public class Program
         
         List<Person> SpecialNamedayDate = NamedayOnApril23rd(FilteredPeople);
         PrintPeople(SpecialNamedayDate, "Alla personer som har namnsdag 23 April");
-        
+
+        //Lista alla namn som börjar på en bestämd bokstavskombination och har namnsdag 
+        //en bestämd månad.
         List<Person> SelectedDateAndStartofName = StartAndDateDecided(FilteredPeople);
         PrintPeople(SelectedDateAndStartofName, "Alla som har namnsdag i Maj och börjar på Ma");
 
-        List<Person> EachStartLetterCount = CountedStartLetter(FilteredPeople);
-        PrintPeople(EachStartLetterCount, "Så här många börjar på resp. bokstav i alfabetet");
         //Skriv en Linq-fråga som listar hur många namn som börjar på varje bokstav i alfabetet.
+        List<object> EachStartLetterCount = CountedStartLetter(FilteredPeople);
+        PrintList(EachStartLetterCount, "Så här många börjar på resp. bokstav i alfabetet");
+        
+
+
     }
 
     static List<Person> ReadFromFile(string path)
@@ -49,12 +55,15 @@ public class Program
                 .ToList();
         }
 
+        //Uppgift 2a
         static List<Person> StartWithSearch(IEnumerable<Person> people)
         {
             return people
-                .Where(static n => n.Name.StartsWith("Li", StringComparison.OrdinalIgnoreCase))
+                .Where(n => n.Name.StartsWith("Li", StringComparison.OrdinalIgnoreCase))
                 .ToList();
         }
+
+        //Uppgift 2b
         static List<Person> NamedayOnApril23rd(IEnumerable<Person> people)
         {
             return people
@@ -62,26 +71,28 @@ public class Program
                 .ToList();
         }
     
+        //Uppgift 2c
         static List<Person> StartAndDateDecided(IEnumerable<Person> people)
         {
             return people
                 .Where(d => d.Namnsdag.Month == 5 && d.Name.StartsWith ("Ma", StringComparison.OrdinalIgnoreCase))
                 .ToList();
         }
-        static List<Person> CountedStartLetter(IEnumerable<Person> people)
-        {
-            return people
-            .GroupBy(p => p.Name.[0]
-            .Select(g => new
+
+
+        //Uppgift 3
+        static List<object> CountedStartLetter(IEnumerable<Person> people)
             {
-                FirstLetter = g.Key,
-                Count = g.Count()
+                return people
+                .Where(p => !string.IsNullOrEmpty(p.Name))
+                .GroupBy(p => p.Name.Substring(0,1))
+                .Select(g => new { FirstLetter = g.Key, Count = g.Count() })
+                .Cast<object>()
+                .ToList();
             }
-            .ToList();
-        }
 
 
-
+    //Olika printmetoder
         static void PrintPeople(List<Person> people, string header)
         {
             Console.WriteLine(header);
@@ -92,16 +103,16 @@ public class Program
             Console.WriteLine($"Antal personer: {people.Count()}");
             Console.WriteLine(".........................");
         }
-        //static void PrintList(List<Person> people, string header)
-        //{
-        //    Console.WriteLine(header);
-        //    foreach (var person in people)
-        //    {
-        //        Console.WriteLine($"{person.Name} {person.Namnsdag}");
-        //    }
-        //    Console.WriteLine($"Antal personer: {people.Count()}");
-        //    Console.WriteLine(".........................");
-        //}
+        static void PrintList(IEnumerable<dynamic> groups, string header)
+        {
+            Console.WriteLine(header);
+            foreach (var group in groups)
+            {
+            Console.WriteLine($"{group.FirstLetter}: {group.Count} personer");
+            }
+            
+            Console.WriteLine(".........................");
+        }
 
 }
 
